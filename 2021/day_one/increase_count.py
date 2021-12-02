69879d3366bcad6b6693635a4708dc278ba8d0a7
nicklovin <43381027+nicklovin@users.noreply.github.com>
2021-12-01 18:59:06 -0800

    Split functions for varied and recycled data + Added sliding window function

--- 2021/day_one/increase_count.py
+++ 2021/day_one/increase_count.py
@@ -4,13 +4,17 @@ cwd = os.getcwd()
 measurement_data_file = '%s/puzzle_inputs/depth_measurements.txt' % os.path.dirname(cwd)
 
 
-def main():
+def get_measurement_data():
     depth_measurements = []
     with open(measurement_data_file) as f:
         for line in f.readlines():
             measure = line.split('\n')[0]
             depth_measurements.append(int(measure))
+    return depth_measurements
 
+
+def get_raw_increasing_measurements():
+    depth_measurements = get_measurement_data()
     greater_measurements = 0
     i = 1
     while i < len(depth_measurements):
@@ -23,5 +27,32 @@ def main():
     return greater_measurements
 
 
+def get_sliding_increasing_measurements():
+    depth_measurements = get_measurement_data()
+    greater_measurements = 0
+    i = 3
+    while i < len(depth_measurements):
+        data_a = depth_measurements[i-3]
+        data_b = depth_measurements[i-2]
+        data_c = depth_measurements[i-1]
+        data_abc = data_a + data_b + data_c
+
+        data_x = depth_measurements[i-2]
+        data_y = depth_measurements[i-1]
+        data_z = depth_measurements[i]
+        data_xyz = data_x + data_y + data_z
+
+        if data_abc < data_xyz:
+            greater_measurements += 1
+        i += 1
+
+    return greater_measurements
+
+
+def main():
+    print(get_raw_increasing_measurements())
+    print(get_sliding_increasing_measurements())
+
+
 if __name__ == '__main__':
-    print(main())
+    main()
