0cd25305d58e3a1fbb6264be9364c052231e7f50
nicklovin <43381027+nicklovin@users.noreply.github.com>
2021-12-01 18:41:12 -0800

    Added count increasing procedures

--- 2021/day_one/increase_count.py
+++ 2021/day_one/increase_count.py
@@ -1,6 +1,27 @@
-main():
-    pass
+import os
+
+cwd = os.getcwd()
+measurement_data_file = '%s/puzzle_inputs/depth_measurements.txt' % os.path.dirname(cwd)
+
+
+def main():
+    depth_measurements = []
+    with open(measurement_data_file) as f:
+        for line in f.readlines():
+            measure = line.split('\n')[0]
+            depth_measurements.append(int(measure))
+
+    greater_measurements = 0
+    i = 1
+    while i < len(depth_measurements):
+        start = depth_measurements[i-1]
+        end = depth_measurements[i]
+        if start < end:
+            greater_measurements += 1
+        i += 1
+
+    return greater_measurements
 
 
 if __name__ == '__main__':
-    main()
+    print(main())
