3c2bfc1f68791abb115a7c06b7990ca46b644c5a
nicklovin <43381027+nicklovin@users.noreply.github.com>
2021-12-02 18:07:18 -0800

    Solution for question 1

--- 2021/day_two/dive_position.py
+++ 2021/day_two/dive_position.py
@@ -1,6 +1,41 @@
+import os
+
+cwd = os.getcwd()
+measurement_data_file = '%s/puzzle_inputs/dive_movements.txt' % os.path.dirname(cwd)
+
+
+def get_movement_data():
+    dive_movements = []
+    with open(measurement_data_file) as f:
+        for line in f.readlines():
+            measure = line.split('\n')[0]
+            dive_movements.append(measure)
+
+    return dive_movements
+
+
+def get_final_positions():
+    depth = 0
+    distance = 0
+    dive_movements = get_movement_data()
+    print(dive_movements)
+
+    for movement in dive_movements:
+        movement_type, delta = movement.split(' ')
+        if movement_type == 'up':
+            depth -= int(delta)
+        elif movement_type == 'down':
+            depth += int(delta)
+        elif movement_type == 'forward':
+            distance += int(delta)
+
+    return distance, depth
+
+
 def main():
-    pass
+    x, y = get_final_positions()
+    return x * y
 
 
 if __name__ == '__main__':
-    main()
+    print(main())
